ed0c18ae287c7237f0219d477d479a6350558ecb
David Mestre <dlmestre@gmail.com>
2015-10-17 14:25:17 +0200

    AWS servers

---
+++ AWSLoadBalancer.py
@@ -0,0 +1,53 @@
+
+import boto.ec2.elb
+import boto
+import sys
+
+AWSAccessKeyId="AKIAJADUQSP3Y6DITLSA"
+AWSSecretKey="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"
+
+
+class AccessLB:
+    def __init__(self,AWSKeyId,AWSSecretKey,number):
+        self.AWSAccessKeyId = AWSKeyId
+        self.AWSSecretKey = AWSSecretKey
+        self.elb_conn = boto.ec2.elb.connect_to_region("us-east-1",
+                                      aws_access_key_id=AWSAccessKeyId,
+                                      aws_secret_access_key=AWSSecretKey)
+        self.ELB = ""
+        self.Instances = ""
+        self.number = number
+        self.SetInstances()
+    def SetInstances(self):
+        if self.number == 1:
+            print(self.elb_conn.get_all_load_balancers())
+            self.ELB = self.elb_conn.get_all_load_balancers()[0]
+            self.Instances = self.ELB.instances
+        elif self.number == 2:
+            self.ELB = self.elb_conn.get_all_load_balancers()[1]
+            self.Instances = self.ELB.instances
+    def GetInstances(self):
+        print("Instances available on your ELB:")
+        print(self.Instances)
+    def AddInstances(self,instance_list):
+        for instance_id in instance_list:
+            try:
+                self.elb_conn.register_instances(self.ELB.name,[instance_id])
+                self.SetInstances()
+                print('Adding %s to ELB %s' % (instance_id, self.ELB.name))
+            except Exception as e:
+                print("Something wrond on your instance")
+                print(e)
+    def RemoveInstances(self,instance_list):
+        for instance_id in instance_list:
+            if instance_id in [i.id for i in self.Instances]:
+                if len(self.Instances) > 1:
+                    self.elb_conn.deregister_instances(self.ELB.name,[instance_id])
+                    self.SetInstances()
+                    print('Removing %s from ELB %s' % (instance_id, self.ELB.name))
+                else:
+                    print("There is only one instance")
+            else:
+                print("Instance %s is not available in ELB %s" % (instance_id, self.ELB.name))
+                
+
---
+++ S3SenderThreading.py
@@ -0,0 +1,68 @@
+import boto
+from boto.s3.connection import S3Connection
+from boto.s3.key import Key
+from boto.s3.connection import OrdinaryCallingFormat
+from filechunkio import FileChunkIO
+import datetime
+import urllib2
+import math
+import os
+import threading
+
+class s3bucket:
+    def __init__(self,path):
+        self.ACCESS_KEY = "AKIAJADUQSP3Y6DITLSA" 
+        self.SECRET_KEY = "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"
+        self.bucket_name = "pqws-sqlbackups"
+        self.path = path
+    def ModificationDate(self,filename):
+        day = os.path.getmtime(filename)
+        return datetime.date.fromtimestamp(day)
+    def ListFiles(self):
+        files = []
+        for name in os.listdir(self.path):
+            if os.path.isfile(os.path.join(self.path,name)):
+                files.append(os.path.join(self.path,name))
+        return files
+    def GetFiles(self):
+        files = []
+        for File in self.ListFiles():
+            filedate = self.ModificationDate(File)
+            if filedate == datetime.date.today():
+                if File.endswith('.bak'):
+                    files.append(File)
+    def Send(self,File):
+        conn = S3Connection(self.ACCESS_KEY,self.SECRET_KEY)
+        bucket = conn.get_bucket(self.bucket_name)
+        chunk_size = 542880000 #500 MG
+        
+        source_size = os.stat(File).st_size
+        print("File Name ", File)
+        print("File Size", source_size)
+        source_size = os.stat(File).st_size
+        if source_size > chunk_size:
+            mp = bucket.initiate_multipart_upload("dbvs_backups/" + str(os.path.basename(File)))
+            chunk_count = int(math.ceil(source_size / float(chunk_size)))
+
+            for i in range(chunk_count):
+                offset = chunk_size * i
+                bytes = min(chunk_size, source_size - offset)
+                with FileChunkIO(File, 'r', offset=offset,bytes=bytes) as fp:
+                    mp.upload_part_from_file(fp, part_num=i + 1)
+            # Finish the upload
+            mp.complete_upload()
+        else:
+            k = Key(bucket)
+            k.name = "dbvs_backups/" + str(os.path.basename(File))
+            k.set_contents_from_filename(File)
+
+    
+    
+path = r"/mnt/sqlbackups"
+
+s3_object = s3bucket(path)
+Files = s3_object.GetFiles()
+print("Files to upload :", Files)
+print("")
+for File in Files:
+    t = threading.Thread(target = s3_object.Send, args=(File,)).start()
